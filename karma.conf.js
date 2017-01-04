06eb6f81edfc656a6f68584d729cff8da0a2bc52
Tiago Coelho <tiagofscoelho@gmail.com>
2017-01-04 22:35:06 +0000

    delete unused files and setup ui router at karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -6,10 +6,9 @@ module.exports = function(config) {
 
     files: [
       'bower_components/angular/angular.js',
-      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-route/angular-ui-router.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'components/**/*.js',
-      'view*/**/*.js'
+      'components/**/*.js'
     ],
 
     autoWatch: true,
